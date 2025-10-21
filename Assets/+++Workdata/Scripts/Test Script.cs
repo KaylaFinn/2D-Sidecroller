34b31cccdea6f2cf39616867a998b90deb46a2a5
KaylaFinn <kayla.breyanna@gmx.de>
2025-10-21 17:42:29 +0200

    2D-Sidecroller Setup
    2D-Sidecroller Setup

---
+++ Assets/+++Workdata/Scripts/Test Script.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    //(optional: Zugriffswert) Variablentypen      Benennung      (optional: Wertzuweisung)
+    public                     float               walkingSpeed    = 5f;
+    
+    /*
+     * inf = ganze Zahlen
+     * float = #Decimalzahlen
+     * string = Zeichenketten -> ABCdef!?10
+     * bool = wahr oder falsch
+     */
+
+    //(optional: Zugriffswert) Variablentypen     Bennenung      (optional : Wertzuweisung)
+    public                     float              walkingSpeed    = 5f;
+
+    public int playerLevel = 5;
+    
+    //booloan
+    public bool isGrounded; // true // false
+
+    public string playerName = "5";
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
